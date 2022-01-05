d15e4061e4ba026ba9720dd16921d80452d8a911
riosgui <guilhermerios100@hotmail.com>
2022-01-05 16:46:53 -0300

    Inicio do uso do Git

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+export const TextInput = ({searchValue, handleChange}) => {
+    return(
+        <input className="text-input" onChange={handleChange}
+        value={searchValue}
+        type="search" 
+        placeholder="Digite sua pesquisa"
+        />
+    )
+}
